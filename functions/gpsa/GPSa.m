3b6efef8c517ed14660ed0f1b78aaf74b26b4bd5
tsgouros <tsgouros@gmail.com>
2019-03-27 11:45:23 -0400

    type

--- functions/gpsa/GPSa.m
+++ functions/gpsa/GPSa.m
@@ -7,7 +7,7 @@ function GPSa(source, event, state)
 % 2012.09.17 - Created
 % 2012.10.05 - Removed excess '/' from state.dir
 % 2013.04.05 - Works as a subsidary of GPS now, updated for GPS1.8
-% 2019.02.14 - CHanged to get state from GPS above.  -ts
+% 2019.02.14 - Changed to get state from GPS above.  -ts
   
 % Save the state
 gpsa_set(state);
