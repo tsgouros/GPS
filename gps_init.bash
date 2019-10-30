f6f5f87d37c6122471e22395a343b0b347d5ad40
Olivia J Newman <ojn@clive.nmr.mgh.harvard.edu>
2019-10-30 11:10:38 -0400

    replaced local file refs to FS and MNE home

--- gps_init.bash
+++ gps_init.bash
@@ -1,11 +1,13 @@
 ## For using GPS, Dave Gow's Granger software
-export FREESURFER_HOME=/Applications/freesurfer
+# export FREESURFER_HOME=/Applications/freesurfer
+export FREESURFER_HOME=/usr/local/freesurfer/stable5_3_0
 source $FREESURFER_HOME/SetUpFreeSurfer.sh
 export FSFAST_HOME=$FREESURFER_HOME/fsfast
 export MNI_DIR=$FREESURFER_HOME/mni
 #setenv SUBJECTS_DIR <Study processed MRI directory>
 # Load the Minimum Norm Estimate software
-export MNE_ROOT=/Applications/MNE-2.7.0-3106-MacOSX-i386
-export MATLAB_ROOT=/Applications/MATLAB_R2018b.app
+#export MNE_ROOT=/Applications/MNE-2.7.0-3106-MacOSX-i386
+export MNE_ROOT=/usr/pubsw/packages/mne/nightly_build
 source $MNE_ROOT/bin/mne_setup_sh
+export MATLAB_ROOT=/Applications/MATLAB_R2018b.app
 ## end GPS stuff
